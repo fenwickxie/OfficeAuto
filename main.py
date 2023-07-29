9124134f2170efe24c3b7e169fe5116da8df936c
VanceXie <vance-xie@outlook.com>
2023-07-30 02:01:48 +0800

    testing

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from FileOperate.Achieve import multithread_winrar_compress
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-	path = r"C:\Users\fy.xie\Desktop\temp\test"
+	path = r"D:\Fenwick\Downloads\7"
 	rename = Rename(path)
 	# DropDuplicates.remove_duplicates(path, 1, 1)
 	# # pre = os.path.basename(path)+'_'
 	# rename.rename_by_sort(path, 1, prefix='', fill_char='0', length=3)
-	# rename.add_prefix_or_suffix(path, '[铃木美咲Misaki Suzuki]', '', ' ')
+	rename.add_prefix_or_suffix('[bouquetman]', '', ' ')
 	# multithread_winrar_compress(path, 27, 64, 2)
-	rename.change_extension('.txt')
+	# rename.change_extension('.7z')
