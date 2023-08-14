10f7c89d888ca3202933048f70a0aed92cc64a3c
VanceXie <vance-xie@outlook.com>
2023-08-14 15:45:38 +0800

    testing

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import os.path
 
 from FileOperate import DropDuplicates, Rename
-from FileOperate.Achieve import multithread_winrar_compress
+from FileOperate.Achieve import multithread_winrar_compress, multithread_winrar_uncompress
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-	path = r"D:\Fenwick\Downloads\7"
-	rename = Rename(path)
-	# DropDuplicates.remove_duplicates(path, 1, 1)
-	# # pre = os.path.basename(path)+'_'
-	# rename.rename_by_sort(path, 1, prefix='', fill_char='0', length=3)
-	rename.add_prefix_or_suffix('[bouquetman]', '', ' ')
-	# multithread_winrar_compress(path, 27, 64, 2)
-	# rename.change_extension('.7z')
+	# base_path = r'D:\ProgramData\Temp\妮妮\游戏\D13-003\files'
+	# for dir in os.listdir(base_path):
+		path = r"E:\Source\Comics\做韵律操的妈妈"
+		# path = os.path.join(base_path, dir)
+		if os.path.isdir(path):
+			rename = Rename(path)
+			# DropDuplicates.remove_duplicates(path, 1, 1)
+			# # pre = os.path.basename(path)+'_'
+			rename.rename_by_sort(0, prefix='', fill_char='0', length=4)
+			# rename.add_prefix_or_suffix(os.path.basename(path), '', ' ')
+			# multithread_winrar_compress(path, 1, 64, 0)
+			# multithread_winrar_uncompress(path,1)
+			# rename.change_extension('.7z')
