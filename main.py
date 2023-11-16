0155615efd596660d95fe66b7607897f6efdcada
VanceXie <vance-xie@outlook.com>
2023-11-17 05:25:13 +0800

    rename extension

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from FileOperate.Achieve import multithread_winrar_compress, multithread_winrar_
 if __name__ == '__main__':
 	# base_path = r'D:\ProgramData\Temp\妮妮\游戏\D13-003\files'
 	# for dir in os.listdir(base_path):
-		path = r"E:\Source\Films"
+		path = r"D:\Fenwick\Downloads\Kidmo"
 		# path = os.path.join(base_path, dir)
 		if os.path.isdir(path):
 			rename = Rename(path)
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 			# # pre = os.path.basename(path)+'_'
 			# rename.rename_by_sort(0, prefix='', fill_char='0', length=4)
 			# rename.add_prefix_or_suffix('[伏娲]', '', ' ')
-			rename.replace_character('-','')
+			# rename.replace_character('-','')
 			# multithread_winrar_compress(path, 1, 64, 0)
 			# multithread_winrar_uncompress(path,1)
-			# rename.change_extension('.7z')
+			rename.change_extension('.rar')
